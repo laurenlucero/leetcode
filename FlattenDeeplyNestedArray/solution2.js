5678ab48aa9f71f9cf26017ecd8656a52265033d
“Lauren <lucerolaurenm@gmail.com>
2023-09-05 17:58:04 -0400

    Delete code to practice writing based on comments

--- FlattenDeeplyNestedArray/solution2.js
+++ FlattenDeeplyNestedArray/solution2.js
@@ -6,22 +6,17 @@
 // Define a function called 'flat' that takes an array 'arr' and an integer 'n' as parameters.
 var flat = function (arr, n) {
   // Define a helper function called 'flattenHelper' within the 'flat' function.
-  function flattenHelper(arr, depth) {
-    // Create an empty array called 'result' to store the flattened elements.
-    let result = [];
-    // Loop through each element in the 'arr' array using a 'for...of' loop.
-    for (let element of arr) {
-      // Check if the 'element' is an array and if the 'depth' is less than 'n'.
-      if (Array.isArray(element) && depth < n) {
-        // Recursively flatten the subarray and append its elements to 'result'.
-        result.push(...flattenHelper(element, depth + 1));
-      } else {
-        // Add the element to 'result' as is (not flattened).
-        result.push(element);
-      }
-    }
-    return result;
-  }
+
+  // Create an empty array called 'result' to store the flattened elements.
+
+  // Loop through each element in the 'arr' array using a 'for...of' loop.
+
+  // Check if the 'element' is an array and if the 'depth' is less than 'n'.
+
+  // Recursively flatten the subarray and append its elements to 'result'.
+
+  // Add the element to 'result' as is (not flattened).
+
   // Start with depth 0 and call the helper function.
   return flattenHelper(arr, 0);
 };
